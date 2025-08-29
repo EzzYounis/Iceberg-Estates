97ab28341088227865956a0cbde16b3f4a50a70d
EZZALDEEN E A YOUNIS <ezzyounis1@gmail.com>
2025-08-29 19:25:01 +0300

    Update CORS configuration to use environment variable for frontend URL

--- api/src/server.js
+++ api/src/server.js
@@ -41,7 +41,7 @@ app.use(helmet({
 
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3001', 'http://localhost:8080'],
+  origin: process.env.FRONTEND_BASE_URL, 
   credentials: true
 }));
 
